b037d905b2791bc2019ce89a492dd4fff27d1ff3
danielkremin <design@kremin.de>
2017-09-10 22:09:53 +0200

    Update Down Button

--- public/site/snippets/down.php
+++ public/site/snippets/down.php
@@ -1,11 +1,11 @@
 
 
-<?php if( $section->hasNext() & $section->next()->isVisible() ): ?>
+<?php if( $section->hasNext() && $section->next()->isVisible() ): ?>
 		<div class="Button Button--down">
 			<?php if( isset($label) ) {
 				echo '<i>'.$label.'</i>';
 			} ?>
-			
+
 			<span>
 				<svg version="1.1" baseProfile="tiny" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
 					 x="0px" y="0px" viewBox="0 0 85.04 48.189" xml:space="preserve">
